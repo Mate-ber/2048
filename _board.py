4edd51d7bcf17a5a32e9001224266d37480f88ae
Mate-ber <72556058+Mate-ber@users.noreply.github.com>
2023-10-20 11:51:15 +0700

    Update _board.py

--- _board.py
+++ _board.py
@@ -1,5 +1,6 @@
 import turtle
 import random
+import time
 STRING_DISTANCE = 100
 
 
@@ -25,7 +26,7 @@ class Logic:
         ycor = 200
         for i in range(0, self.board_height):
             for j in range(0, self.board_width):
-                self.tur.write(self.cubs[i][j], align="left", font=("Courier", 20, "normal"))
+                self.tur.write(self.cubs[i][j], align="left", font=("Courier", 25, "normal"))
                 xcor += STRING_DISTANCE
                 self.tur.goto(xcor, ycor)
             xcor -= (4 * STRING_DISTANCE)
@@ -43,95 +44,87 @@ class Logic:
         self.print()
 
     def up(self):
-        for j in range(self.board_width):
-            for i in range(self.board_height-1, 0, -1):
-                if self.cubs[i][j] != 0:
-                    if self.cubs[i-1][j] == 0:
-                        self.cubs[i-1][j] = self.cubs[i][j]
-                        self.cubs[i][j] = 0
-
+        self.move_up()
         for j in range(self.board_width):
             for i in range(self.board_height-1):
                 if self.cubs[i][j] != 0:
                     if self.cubs[i][j] == self.cubs[i+1][j]:
                         self.cubs[i][j] += self.cubs[i+1][j]
                         self.cubs[i+1][j] = 0
-
-        for j in range(self.board_width):
-            for i in range(self.board_height - 1, 0, -1):
-                if self.cubs[i][j] != 0:
-                    if self.cubs[i - 1][j] == 0:
-                        self.cubs[i - 1][j] = self.cubs[i][j]
-                        self.cubs[i][j] = 0
+        self.move_up()
         self.generate()
 
-    def down(self):
+    def move_up(self):
         for j in range(self.board_width):
-            for i in range(self.board_height-1):
-                if self.cubs[i][j] != 0:
-                    if self.cubs[i+1][j] == 0:
-                        self.cubs[i+1][j] = self.cubs[i][j]
-                        self.cubs[i][j] = 0
+            for i in range(self.board_height):
+                z = i
+                while z > 0 and self.cubs[z-1][j] == 0:
+                    self.cubs[z-1][j] = self.cubs[i][j]
+                    self.cubs[i][j] = 0
+                    z -= 1
 
+    def down(self):
+        self.move_down()
         for j in range(self.board_width):
             for i in range(self.board_height-1, 0, -1):
                 if self.cubs[i][j] != 0:
                     if self.cubs[i][j] == self.cubs[i-1][j]:
                         self.cubs[i][j] += self.cubs[i-1][j]
                         self.cubs[i-1][j] = 0
-        for j in range(self.board_width):
-            for i in range(self.board_height-1):
-                if self.cubs[i][j] != 0:
-                    if self.cubs[i+1][j] == 0:
-                        self.cubs[i+1][j] = self.cubs[i][j]
-                        self.cubs[i][j] = 0
+        self.move_down()
         self.generate()
 
-    def left(self):
-        for i in range(self.board_height):
-            for j in range(self.board_width - 1, 0, -1):
+    def move_down(self):
+        for j in range(self.board_width):
+            for i in range(self.board_height-1, -1, -1):
                 if self.cubs[i][j] != 0:
-                    if self.cubs[i][j - 1] == 0:
-                        self.cubs[i][j - 1] = self.cubs[i][j]
+                    z = i
+                    while z < self.board_height-1 and self.cubs[z + 1][j] == 0:
+                        self.cubs[z + 1][j] = self.cubs[i][j]
                         self.cubs[i][j] = 0
+                        z += 1
 
+    def left(self):
+        self.move_left()
         for i in range(self.board_height):
             for j in range(self.board_width - 1):
                 if self.cubs[i][j] != 0:
                     if self.cubs[i][j] == self.cubs[i][j + 1]:
                         self.cubs[i][j] += self.cubs[i][j + 1]
                         self.cubs[i][j + 1] = 0
-
-        for i in range(self.board_height):
-            for j in range(self.board_width - 1, 0, -1):
-                if self.cubs[i][j] != 0:
-                    if self.cubs[i][j - 1] == 0:
-                        self.cubs[i][j - 1] = self.cubs[i][j]
-                        self.cubs[i][j] = 0
+        self.move_left()
         self.generate()
 
-    def right(self):
+    def move_left(self):
         for i in range(self.board_height):
-            for j in range(self.board_width - 1):
+            for j in range(self.board_width):
                 if self.cubs[i][j] != 0:
-                    if self.cubs[i][j + 1] == 0:
-                        self.cubs[i][j + 1] = self.cubs[i][j]
+                    z = j
+                    while z > 0 and self.cubs[i][z-1] == 0:
+                        self.cubs[i][z-1] = self.cubs[i][j]
                         self.cubs[i][j] = 0
+                        z -= 1
 
+    def right(self):
+        self.move_right()
         for i in range(self.board_height):
             for j in range(self.board_width - 1, 0, -1):
                 if self.cubs[i][j] != 0:
                     if self.cubs[i][j] == self.cubs[i][j - 1]:
                         self.cubs[i][j] += self.cubs[i][j - 1]
                         self.cubs[i][j - 1] = 0
-
-        for i in range(self.board_height):
-            for j in range(self.board_width - 1):
-                if self.cubs[i][j] != 0:
-                    if self.cubs[i][j + 1] == 0:
-                        self.cubs[i][j + 1] = self.cubs[i][j]
-                        self.cubs[i][j] = 0
+        self.move_right()
         self.generate()
+
+    def move_right(self):
+        for i in range(self.board_height):
+            for j in range(self.board_width - 1, -1, -1):
+                if self.cubs[i][j] != 0:
+                    z = j
+                    while z < self.board_width-1 and self.cubs[i][z+1] == 0:
+                        self.cubs[i][z+1] = self.cubs[i][j]
+                        self.cubs[i][j] = 0
+                        z += 1
 
     def if_win(self):
         for i in range(self.board_height):
@@ -141,6 +134,7 @@ class Logic:
         return False
 
     def print_win(self):
+        time.sleep(0.3)
         self.tur.clear()
         win = turtle.Turtle()
         win.color("black")
@@ -164,6 +158,7 @@ class Logic:
         return True
 
     def print_lose(self):
+        time.sleep(0.3)
         self.tur.clear()
         lose = turtle.Turtle()
         lose.color("black")
